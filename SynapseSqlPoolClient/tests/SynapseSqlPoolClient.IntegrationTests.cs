ade8b5cc4bb3515c2d8a43028899e9a188fce37a
Dinake, Keoagile <kdinake@deloitte.co.za>
2025-09-10 10:38:36 +0200

    refactor sql pool client to use sql conn factory with support for various auth modes

--- SynapseSqlPoolClient/tests/SynapseSqlPoolClient.IntegrationTests.cs
+++ SynapseSqlPoolClient/tests/SynapseSqlPoolClient.IntegrationTests.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
-using Synapsical.Synapse.SqlPool.Client;
-using Azure.Identity;
 
 namespace Synapsical.Synapse.SqlPool.Client.Tests
 {
@@ -11,13 +9,69 @@ namespace Synapsical.Synapse.SqlPool.Client.Tests
     {
         // Set these to your test Synapse SQL Pool endpoint and credentials for real integration tests
         private const string SqlPoolEndpoint = "<your-synapse-sqlpool-endpoint>";
+        private const string Database = "<your-database>";
         private const string Username = "<your-username>";
         private const string Password = "<your-password>";
+        private const string AadUser = "<aad-user>";
+        private const string AadPassword = "<aad-password>";
+        private const string ClientId = "<client-id>";
+        private const string TenantId = "<tenant-id>";
+        private const string ClientSecret = "<client-secret>";
 
         [Fact(Skip = "Set real endpoint and credentials to run integration test.")]
         public async Task FullCrudLifecycle_Works()
         {
-            var client = new SynapseSqlPoolClient(SqlPoolEndpoint, Username, Password);
+            // Example: SQL Password
+            var client = new SynapseSqlPoolClient(
+                SqlPoolEndpoint,
+                Database,
+                SqlAuthMode.SqlPassword,
+                username: Username,
+                password: Password
+            );
+
+            // Example: Active Directory Password
+            // var client = new SynapseSqlPoolClient(
+            //     SqlPoolEndpoint,
+            //     Database,
+            //     SqlAuthMode.ActiveDirectoryPassword,
+            //     username: AadUser,
+            //     password: AadPassword
+            // );
+
+            // Example: Active Directory Integrated
+            // var client = new SynapseSqlPoolClient(
+            //     SqlPoolEndpoint,
+            //     Database,
+            //     SqlAuthMode.ActiveDirectoryIntegrated
+            // );
+
+            // Example: Active Directory Interactive
+            // var client = new SynapseSqlPoolClient(
+            //     SqlPoolEndpoint,
+            //     Database,
+            //     SqlAuthMode.ActiveDirectoryInteractive,
+            //     username: AadUser
+            // );
+
+            // Example: Active Directory Service Principal
+            // var client = new SynapseSqlPoolClient(
+            //     SqlPoolEndpoint,
+            //     Database,
+            //     SqlAuthMode.ActiveDirectoryServicePrincipal,
+            //     clientId: ClientId,
+            //     password: ClientSecret,
+            //     tenantId: TenantId
+            // );
+
+            // Example: AccessToken (DefaultAzureCredential)
+            // var credential = new DefaultAzureCredential();
+            // var client = new SynapseSqlPoolClient(
+            //     SqlPoolEndpoint,
+            //     Database,
+            //     SqlAuthMode.AccessToken,
+            //     credential: credential
+            // );
             var tableName = "TestTable";
             var schema = "(Id INT PRIMARY KEY, Name NVARCHAR(100))";
 
--- SynapseSqlPoolClient/tests/SynapseSqlPoolClient.UnitTests.cs
+++ SynapseSqlPoolClient/tests/SynapseSqlPoolClient.UnitTests.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
-using Synapsical.Synapse.SqlPool.Client;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Azure.Core;
 
@@ -11,25 +11,119 @@ namespace Synapsical.Synapse.SqlPool.Client.Tests
     public class SynapseSqlPoolClientUnitTests
     {
         [Fact]
-        public async Task TableExistsAsync_ReturnsFalse_WhenTableDoesNotExist()
+        public void Can_Construct_With_All_Auth_Modes()
         {
-            // This is a placeholder. In a real test, mock SqlConnection/SqlCommand.
-            var client = new SynapseSqlPoolClient("fake-server", "user", "pass");
-            await Assert.ThrowsAsync<Microsoft.Data.SqlClient.SqlException>(() => client.TableExistsAsync("NonExistentTable"));
+            var mockFactory = new Mock<ISqlConnectionFactory>().Object;
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>().Object;
+            // SQL Password
+            var c1 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock);
+            // AD Password
+            var c2 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.ActiveDirectoryPassword, "aaduser", "aadpass", logger: loggerMock);
+            // AD Integrated
+            var c3 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.ActiveDirectoryIntegrated, logger: loggerMock);
+            // AD Interactive
+            var c4 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.ActiveDirectoryInteractive, "aaduser", logger: loggerMock);
+            // AD Service Principal
+            var c5 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.ActiveDirectoryServicePrincipal, clientId: "cid", password: "secret", tenantId: "tid", logger: loggerMock);
+            // AccessToken
+            var credential = new Mock<TokenCredential>().Object;
+            var c6 = new SynapseSqlPoolClient("server", "master", SqlAuthMode.AccessToken, credential: credential, logger: loggerMock);
+            // With custom factory
+            var c7 = new SynapseSqlPoolClient("server", mockFactory, loggerMock);
+        }
+        [Fact]
+        public void Constructor_ThrowsArgumentException_OnEmptyEndpoint()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            Assert.Throws<ArgumentException>(() => new SynapseSqlPoolClient("", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object));
         }
 
         [Fact]
-        public async Task CreateTableAsync_ThrowsException_OnInvalidSql()
+        public void Constructor_ThrowsArgumentException_OnEmptyUsernameOrPassword()
         {
-            var client = new SynapseSqlPoolClient("fake-server", "user", "pass");
-            await Assert.ThrowsAsync<Microsoft.Data.SqlClient.SqlException>(() => client.CreateTableAsync("BadTable", "(Bad SQL)"));
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            Assert.Throws<ArgumentException>(() => new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "", "pass", logger: loggerMock.Object));
+            Assert.Throws<ArgumentException>(() => new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "", logger: loggerMock.Object));
         }
 
         [Fact]
-        public async Task InsertRowAsync_ThrowsException_OnNullTable()
+        public void Constructor_ThrowsArgumentNullException_OnNullCredential()
         {
-            var client = new SynapseSqlPoolClient("fake-server", "user", "pass");
-            await Assert.ThrowsAsync<Microsoft.Data.SqlClient.SqlException>(() => client.InsertRowAsync("", new Dictionary<string, object> { { "Id", 1 } }));
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            Assert.Throws<ArgumentNullException>(() => new SynapseSqlPoolClient("server", "master", SqlAuthMode.AccessToken, credential: null, logger: loggerMock.Object));
+        }
+
+        [Fact]
+        public async Task CreateTableAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.CreateTableAsync(null, "(Id INT)"));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.CreateTableAsync("", "(Id INT)"));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.CreateTableAsync("Table", null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.CreateTableAsync("Table", ""));
+        }
+
+        [Fact]
+        public async Task InsertRowAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.InsertRowAsync(null, new Dictionary<string, object> { { "Id", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.InsertRowAsync("", new Dictionary<string, object> { { "Id", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.InsertRowAsync("Table", null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.InsertRowAsync("Table", new Dictionary<string, object>()));
+        }
+
+        [Fact]
+        public async Task UpdateRowsAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync(null, "where", new Dictionary<string, object> { { "Col", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync("", "where", new Dictionary<string, object> { { "Col", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync("Table", null, new Dictionary<string, object> { { "Col", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync("Table", "", new Dictionary<string, object> { { "Col", 1 } }));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync("Table", "where", null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.UpdateRowsAsync("Table", "where", new Dictionary<string, object>()));
+        }
+
+        [Fact]
+        public async Task DeleteRowsAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.DeleteRowsAsync(null, "where"));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.DeleteRowsAsync("", "where"));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.DeleteRowsAsync("Table", null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.DeleteRowsAsync("Table", ""));
+        }
+
+        [Fact]
+        public async Task TableExistsAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.TableExistsAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.TableExistsAsync(""));
+        }
+
+        [Fact]
+        public async Task ListTablesAsync_DoesNotThrow()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            // This will throw SqlException if run, but we just want to check the method exists and is callable
+            await Assert.ThrowsAnyAsync<Exception>(() => client.ListTablesAsync());
+        }
+
+        [Fact]
+        public async Task QueryAsync_ThrowsArgumentException_OnInvalidArgs()
+        {
+            var loggerMock = new Mock<ILogger<SynapseSqlPoolClient>>();
+            var client = new SynapseSqlPoolClient("server", "master", SqlAuthMode.SqlPassword, "user", "pass", logger: loggerMock.Object);
+            await Assert.ThrowsAsync<ArgumentException>(() => client.QueryAsync(null));
+            await Assert.ThrowsAsync<ArgumentException>(() => client.QueryAsync(""));
         }
     }
 }
